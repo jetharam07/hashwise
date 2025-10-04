53610b07adf95d747f06d545c8c64e18fb555000
Jetharam Gehlot <160430686+jetharam07@users.noreply.github.com>
2025-10-05 01:03:32 +0530

    Update Contact.jsx

--- components/homes/index-corporate-one-page/Contact.jsx
+++ components/homes/index-corporate-one-page/Contact.jsx
@@ -168,8 +168,10 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        <div className="row g-5">
-          <div className="col-lg-7">
+
+        {/* Contact Form Full Width */}
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
             <form
               ref={formRef}
               className="contact-form-1 rainbow-dynamic-form"
@@ -214,11 +216,10 @@ export default function Contact() {
                   name="message"
                   id="contact-message"
                   placeholder="Your Message"
-                  defaultValue={""}
                   required
                 />
               </div>
-              <div className="form-group">
+              <div className="form-group text-center">
                 <button
                   name="submit"
                   type="submit"
@@ -229,20 +230,8 @@ export default function Contact() {
                   <span>{loading ? "Sending..." : "Submit Now"}</span>
                 </button>
               </div>
-              {success && <p className="mt-3">{success}</p>}
+              {success && <p className="mt-3 text-center">{success}</p>}
             </form>
-          </div>
-          <div className="col-lg-5">
-            <div className="google-map-style-1">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14554259.179133086!2d-105.54385245388013!3d37.49334218664659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sbd!4v1630777307491!5m2!1sen!2sbd"
-                width={600}
-                height={550}
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy"
-              />
-            </div>
           </div>
         </div>
       </div>
